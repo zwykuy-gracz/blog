0fd131efa756af57661b2725b7d079bcc9111958
paul <paul@LAPTOP-3LBQERML>
2025-02-12 18:34:55 +0100

    added Markdown to work with prism.js

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     }
 }
 
-DATABASES["default"] = dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# DATABASES["default"] = dj_database_url.parse(os.environ.get("DATABASE_URL"))
 
 
 # Password validation
